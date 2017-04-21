7c9f88479732724f042189171e4d2cfad5154fb5
stemlatam <asilva@stemlatam.com>
2017-04-20 22:55:58 -0300

    Create stemget.py
    Python Project for IoT, learning ...

---
+++ stemget.py
@@ -0,0 +1,24 @@
+curso_stem_iot = "Aprendiendo Python 3"
+curso_stem_iot = 90
+curso_stem_iot = True 
+print( curso_stem_iot )
+number_one = 10
+number_two = 4
+
+result = number_one + number_two
+print ("suma", result)
+
+result = number_one - number_two
+print ("resta", result)
+
+result = number_one * number_two
+print ("multiplica", result)
+
+result = number_one / number_two #Regresa numero flotante
+print ("divide", result)
+
+result = number_one // number_two #Regresa numero entero
+print ("divide", result)
+
+result = number_one ** number_two #Regresa numero a su exponencial
+print ("Exponencial", result)
